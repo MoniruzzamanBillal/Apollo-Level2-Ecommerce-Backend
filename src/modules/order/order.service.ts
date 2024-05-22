054ed2faece37d9523c768543b897ca390aaf8a4
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-05-22 15:50:28 +0600

    order : insufficient functionality added

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,8 +1,27 @@
+import { productServices } from "../product/product.service";
 import { Torder } from "./order.interface";
 import orderModel from "./order.model";
 
 // ! create order into DBb
 const createOrderInDB = async (orderData: Torder) => {
+  const productData = await productServices.getSinglePorductDB(
+    orderData.productId
+  );
+
+  if (!productData) {
+    throw new Error("Product not found");
+  }
+
+  if (orderData.quantity > productData?.inventory?.quantity) {
+    throw new Error("Insufficient stock");
+  }
+
+  productData.inventory.quantity -= orderData.quantity;
+  productData.inventory.inStock =
+    productData.inventory.quantity > 0 ? true : false;
+
+  await productData.save();
+
   const response = await orderModel.create(orderData);
 
   return response;
